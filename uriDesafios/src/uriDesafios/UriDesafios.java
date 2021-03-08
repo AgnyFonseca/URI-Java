455a394d65da9aa1b1d15ff41690dd9d0eebe9c3
Agny Fonseca <70056921+AgnyFonseca@users.noreply.github.com>
2021-03-08 18:15:44 +0100

    New URI

--- uriDesafios/src/uriDesafios/UriDesafios.java
+++ uriDesafios/src/uriDesafios/UriDesafios.java
@@ -836,6 +836,50 @@ public class UriDesafios {
 		System.out.println(maior);
 		System.out.println(posicao);*/
 		
+		//URI 1094
+		/*int n = sc.nextInt();
+		int coelho = 0;
+		int rato = 0;
+		int sapo = 0;
+		String x = " ";
+		int aux = 0;
+		int total = 0;
+		double percentC, percentR, percentS;
+		
+		for (int i = 1; i <= n; i++) {
+			aux = sc.nextInt();
+			x = sc.next();
+			switch(x) {
+			case "C":
+				coelho += aux;
+				break;
+			case "R":
+				rato += aux;
+				break;
+			case "S":
+				sapo += aux;
+				break;
+			}
+			
+			total = coelho + rato + sapo;
+			
+		}
+		
+		percentC = coelho * 100.00 / total;
+		percentR = rato * 100.00 / total;
+		percentS = sapo * 100.00 / total;
+		
+		System.out.printf("Total: %d cobaias%n", total);
+		System.out.printf("Total de coelhos: %d%n", coelho);
+		System.out.printf("Total de ratos: %d%n", rato);
+		System.out.printf("Total de sapos: %d%n", sapo);
+		System.out.printf("Percentual de coelhos: %.2f", percentC);
+		System.out.print(" %\n");
+		System.out.printf("Percentual de ratos: %.2f", percentR);
+		System.out.print(" %\n");
+		System.out.printf("Percentual de sapos: %.2f", percentS);
+		System.out.print(" %\n");
+		*/
 		
 		sc.close();
 	}
