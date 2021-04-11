d819a2c6f2058664917fe5229dcac292dd8c3f2d
Agny Fonseca <70056921+AgnyFonseca@users.noreply.github.com>
2021-04-11 18:58:05 +0200

    newURI

--- uriDesafios/src/uriDesafios/UriDesafios.java
+++ uriDesafios/src/uriDesafios/UriDesafios.java
@@ -1458,6 +1458,26 @@ public class UriDesafios {
 			}
 		} */
 
+		//URI 1164
+		/*int n = sc.nextInt();
+		int perfeito, x;
+		
+		for (int i = 1; i <= n; i++) {
+			perfeito = 0;
+			x = sc.nextInt();
+			
+			for (int j = 1; j < x; j++) {
+				if (x % j == 0) {
+					perfeito += j;
+				}
+			}
+			if (perfeito == x) {
+				System.out.println(x + " eh perfeito");
+			} else {
+				System.out.println(x + " nao eh perfeito");
+			}
+		} */
+
 		
 		sc.close();
 	}
