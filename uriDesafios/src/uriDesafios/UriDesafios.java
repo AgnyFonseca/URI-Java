ecd00741e89a741d7e312b52804fa8ae7be00123
Agny Fonseca <70056921+AgnyFonseca@users.noreply.github.com>
2021-04-11 17:21:00 +0200

    newURI

--- uriDesafios/src/uriDesafios/UriDesafios.java
+++ uriDesafios/src/uriDesafios/UriDesafios.java
@@ -1432,6 +1432,32 @@ public class UriDesafios {
 			x = sc.nextInt();
 		} */
 
+		//URI 1160
+		/*int teste = sc.nextInt();
+		int pa, pb, anos;
+		double g1, g2;
+		
+		for (int i = 1; i <= teste; i++) {
+			anos = 0;
+			pa = sc.nextInt();
+			pb = sc.nextInt();
+			g1 = sc.nextDouble();
+			g2 = sc.nextDouble();
+			while (pb >= pa) {
+				pa += (pa * g1) / 100;
+				pb += (pb * g2) / 100;
+				anos++;
+				if (anos > 100) {
+					break;
+				}
+			}
+			if (anos > 100) {
+				System.out.println("Mais de 1 seculo.");
+			} else {
+				System.out.println(anos + " anos.");
+			}
+		} */
+
 		
 		sc.close();
 	}
