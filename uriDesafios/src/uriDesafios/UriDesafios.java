136a426a0d70250e79d089ba8a97118738aaf181
Agny Fonseca <70056921+AgnyFonseca@users.noreply.github.com>
2021-04-22 21:39:57 +0200

    newURI

--- uriDesafios/src/uriDesafios/UriDesafios.java
+++ uriDesafios/src/uriDesafios/UriDesafios.java
@@ -1583,6 +1583,83 @@ public class UriDesafios {
 				System.out.printf("N[%d] = %.4f%n", i, n[i]);
 			}
 		} */
+
+		//URI 1179
+		/*int[] par = new int[5];
+		int[] impar = new int[5];
+		int cPar = 0, cImpar = 0;
+		
+		for (int i = 0; i < 15; i++) {
+			if(cPar == 5) {
+				for (int k = 0; k < cPar; k++) {
+					System.out.println("par[" + k + "] = " + par[k]);
+				}
+				cPar = 0;
+			
+			} else if(cImpar == 5) {
+				for (int j = 0; j < cImpar; j++) {
+					System.out.println("impar[" + j + "] = " + impar[j]);
+				}
+				cImpar = 0;
+			}
+			
+			int n = sc.nextInt();
+			
+			if (n % 2 == 0) {
+				par[cPar] = n;
+				cPar++;
+			
+			} else {
+				impar[cImpar] = n;
+				cImpar++;
+			}
+		}
+		
+		for (int i = 0; i < cImpar; i++) {
+			System.out.println("impar[" + i + "] = " + impar[i]);
+		}
+		
+		for (int i = 0; i < cPar; i++) {
+			System.out.println("par[" + i + "] = " + par[i]);
+		} */
+		
+		//URI 1179
+		/*int x, cPar = 0, cImpar = 0;
+		int[] par = new int[5];
+		int[] impar = new int[5];
+		
+		for (int i = 0; i < 15; i++) {
+			x = sc.nextInt();
+			
+			if (x % 2 == 0) {
+				par[cPar] = x;
+				cPar++;
+			} else {
+				impar[cImpar] = x;
+				cImpar++;
+			}
+			
+			if (cPar == 5) {
+				cPar = 0;
+				for (int j = 0; j < par.length; j++) {
+					System.out.println("par[" + j + "] = " + par[j]);
+				}
+				
+			} else if (cImpar == 5) {
+				cImpar = 0;
+				for (int j = 0; j < impar.length; j++) {
+					System.out.println("impar[" + j + "] = " + impar[j]);
+				}
+			}
+		}
+		
+		for (int i = 0; i < cImpar; i++) {
+			System.out.println("impar[" + i + "] = " + impar[i]);
+		}
+		
+		for (int i = 0; i < cPar; i++) {
+			System.out.println("par[" + i + "] = " + par[i]);
+		} */
 		
 		sc.close();
 	}
