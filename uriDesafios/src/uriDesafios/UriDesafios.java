96cfaad42f57705608fcf157d33f3b0f41ee4624
Agny <agnyfonseca@outlook.com>
2021-08-14 11:34:56 +0200

    newUri

--- uriDesafios/src/uriDesafios/UriDesafios.java
+++ uriDesafios/src/uriDesafios/UriDesafios.java
@@ -1980,6 +1980,40 @@ public class UriDesafios {
 			}
 		} */
 		
+		//URI 1557
+		/*int n = sc.nextInt();
+		int x, t, y;
+
+		while (n != 0) {
+
+			int[][] mat = new int[n][n];
+			x = 1;
+
+			for (int i = 1; i <= n; i++) {
+				y = x;
+				for (int j = 1; j <= n; j++) {
+					mat[i - 1][j - 1] = y;
+					y *= 2;
+				}
+				x *= 2;
+			}
+
+			t = String.valueOf(mat[n - 1][n - 1]).length();
+
+			for (int i = 1; i <= n; i++) {
+				for (int j = 1; j <= n; j++) {
+					System.out.printf("%" + t + "d", mat[i - 1][j - 1]);
+					if (j < n) {
+						System.out.print(" ");
+					} else {
+						System.out.print("\n");
+					}
+				}
+			}
+			System.out.print("\n");
+			n = sc.nextInt();
+		} */
+		
 		sc.close();
 	}
 }
